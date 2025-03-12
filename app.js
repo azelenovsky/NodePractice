85e643d3f701ddb2793989e9960e4cc5f933a82e
azelenovsky <30359132+azelenovsky@users.noreply.github.com>
2025-03-12 09:08:39 -0400

    Main layout and commiting

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req,res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,"views", "404.html"))
+    res.status(400).render('404');
+//    res.status(404).sendFile(path.join(__dirname,"views", "404.html"))
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
