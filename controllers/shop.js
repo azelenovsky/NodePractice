4648f0f1c7fe20258a4f1ab16ce9cc8a6e9df062
azelenovsky <30359132+azelenovsky@users.noreply.github.com>
2025-03-23 22:41:55 -0400

    Dynamic id for product

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,19 @@ const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-    res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
-    })
-});
+        res.render('shop/product-list', {
+            prods: products,
+            pageTitle: 'All Products',
+            path: '/products'
+        });
+    });
 };
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    console.log(prodId);
+    res.redirect('/');
+}
 
 exports.getIndex = (req, res, next) => {
     Product.fetchAll(products => {
--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,8 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/chart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
