e7582f17b6dbb8a840b6b6e44059374ff7635c86
azelenovsky <30359132+azelenovsky@users.noreply.github.com>
2025-04-04 18:03:46 -0400

    Cart works, price concat issue

--- models/cart.js
+++ models/cart.js
@@ -16,7 +16,7 @@ module.exports = class Cart {
                 cart = JSON.parse(fileContent);
             }
             // Analyze the cart => Find existing product
-            const existingProductIndex = cart.products.find(prod => prod.id === id);
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
             const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
             // Add new product/increase quantity
@@ -27,13 +27,13 @@ module.exports = class Cart {
                 cart.products[existingProductIndex] = updatedProduct;
             } else {
                 updatedProduct = {id: id, qty: 1};
-                cart.products = [...cart.products];
+                cart.products = [...cart.products, updatedProduct];
             }
             cart.totalPrice = cart.totalPrice + productPrice;
             fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log("this?");
                 console.log(err);
-            });
-            
+            });            
     });
 
 }
