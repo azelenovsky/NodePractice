6fa7c221663226fd5071dc509b484a9746425ad2
azelenovsky <30359132+azelenovsky@users.noreply.github.com>
2025-04-07 11:06:37 -0400

    Update product WIP

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,9 +32,20 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageURL;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageURL = req.body.imageURL;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(
+        prodId, updatedTitle, updatedImageURL, updatedDesc, updatedPrice);
+    updatedProduct.save();
 };
 
 exports.getProducts = (req, res, next) => {
