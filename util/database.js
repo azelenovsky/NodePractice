0f80af769c17f68d14ac1ee8da61a3ca26720bd7
azelenovsky <30359132+azelenovsky@users.noreply.github.com>
2025-04-27 18:22:47 -0400

    Database works

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPoolCluster({
+const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     database: "node-complete",
